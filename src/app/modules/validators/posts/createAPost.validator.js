76689d566e660500407d4acb9d74d3781a92c8e6
chaderenyore <65408156+chaderenyore@users.noreply.github.com>
2023-04-17 21:19:03 +0100

    Fix - make post title optional (#36)
    Add more editor config files to gitignore

--- src/app/modules/validators/posts/createAPost.validator.js
+++ src/app/modules/validators/posts/createAPost.validator.js
@@ -2,6 +2,6 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 exports.createPostsSchema = Joi.object().keys({
-  post_title: Joi.string().required(),
+  post_title: Joi.string().optional(),
   post_body_text: Joi.string().required(),
 });
