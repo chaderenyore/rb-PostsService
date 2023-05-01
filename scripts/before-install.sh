91417afcba84dc2a7a7f0ac0cd9a60b3bbd2a9c9
ADJARO OGAGA <45229900+ogagaCodes@users.noreply.github.com>
2023-05-01 12:21:46 +0100

    Update before-install.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -4,6 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+
 cd /home/ubuntu/services/Posts
 rm -rf package-lock.json
 npm install @aws-sdk/client-s3 --save
